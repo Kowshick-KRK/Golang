27f44c4467127c51a8dcdc7e46e9277c35ce3493
Kowshick-KRK <71482636+Kowshick-KRK@users.noreply.github.com>
2021-02-04 11:25:31 +0530

    Add files via upload

---
+++ TCP/Client.go
@@ -0,0 +1,35 @@
+package main
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+func main() {
+	arguments := os.Args
+	if(len(arguments) == 1) {
+		fmt.Println("Connect to host:9000")
+		return
+	}
+CONNECT := arguments[1]
+c,err := net.Dial("tcp", CONNECT)
+if err != nil {
+	fmt.Println(err)
+	return 
+}
+for {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(">>")
+	text, _ := reader.ReadString('\n')
+	fmt.Fprintf(c, text+"\n")
+
+	message, _ := bufio.NewReader(c).ReadString('\n')
+	fmt.Print("->: ", message) 
+	if strings.TrimSpace(string(text)) == "STOP" {
+		fmt.Println("TCP connection Exiting")
+		return
+	}
+
+}
+}
